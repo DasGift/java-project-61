94212673f98a8ac53ecd0f32ba133b9c9bb8061b
DasGift <evg2037@yandex.ru>
2023-07-27 02:11:08 +0700

    Первый вариант проекта

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,49 @@
 package hexlet.code;
 
+import hexlet.code.Games.*;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        StringBuilder allGames = new StringBuilder();
+        System.out.println("Please enter the game number and press Enter.");
+        allGames.append("1 - Greet");
+        allGames.append("\n" + "2 - Even");
+        allGames.append("\n" + "3 - Calc");
+        allGames.append("\n" + "4 - GDC");
+        allGames.append("\n" + "5 - Progression");
+        allGames.append("\n" + "6 - Prime");
+        allGames.append("\n" + "0 - Exit");
+        System.out.println(allGames.toString());
+        System.out.print("Your choice: ");
+        Scanner scStart = new Scanner(System.in);
+        int gameNumber = scStart.nextInt();
+        switch (gameNumber) {
+            case 0:
+                break;
+            case 1:
+                Cli.sayHello();
+                break;
+            case 2:
+                Cli.sayHello();
+                Even.gameEven();
+                break;
+            case 3:
+                Cli.sayHello();
+                Calculator.gameCalc();
+                break;
+            case 4:
+                Cli.sayHello();
+                GCD.gcdGame();
+            case 5:
+                Cli.sayHello();
+                MathProgress.mathPrGame();
+            case 6:
+                Cli.sayHello();
+                Prime.primeGame();
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Games/GCD.java
@@ -0,0 +1,40 @@
+package hexlet.code.Games;
+
+import hexlet.code.Cli;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void gcdGame() {
+        Scanner scAnswer = new Scanner(System.in);
+        String wrongAnswer = "' is wrong answer ;(. Correct answer was '";
+        int howMuchCorrect = 0;
+        final int maxAnswers = 3;
+        final int borderForRandom = 100;
+        System.out.println("Find the greatest common divisor of given numbers.");
+        while (howMuchCorrect < maxAnswers) {
+            int minDel = 0;
+            int firstNum = (int) (Math.random() * borderForRandom);
+            int secondNum = (int) (Math.random() * borderForRandom);
+            System.out.println("Question: " + firstNum + " " + secondNum);
+            System.out.print("Your answer: ");
+            while (secondNum != 0) {
+                minDel = secondNum;
+                secondNum = firstNum % secondNum;
+                firstNum = minDel;
+            }
+            int answer = scAnswer.nextInt();
+            if (answer == minDel) {
+                System.out.println("Correct!");
+                howMuchCorrect++;
+            } else {
+                System.out.println("'" + answer + wrongAnswer + minDel + "'.");
+                Cli.sayBye();
+                break;
+            }
+        }
+        if (howMuchCorrect == maxAnswers) {
+            Cli.winGame();
+        }
+    }
+}
